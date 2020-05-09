349bf7068bd53c05263c5afde4458854757342b7
pranshul2112 <63261270+pranshul2112@users.noreply.github.com>
2020-05-09 23:14:20 +0530

    Update Sorting: Comparator.py

--- Sorting: Comparator.py
+++ Sorting: Comparator.py
@@ -16,3 +16,15 @@ class Player:
         return 1 if a.score < b.score else -1
 
 
+n = int(input())
+data = []
+for i in range(n):
+    name, score = input().split()
+    score = int(score)
+    player = Player(name, score)
+    data.append(player)
+    
+data = sorted(data, key=cmp_to_key(Player.comparator))
+for i in data:
+    print(i.name, i.score)
+
